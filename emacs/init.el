11f971ab20ee18c017c3227bd83447308369e408
Marcin Pajkowski <marcin.pajkowski@gmail.com>
2022-11-01 09:39:29 +0100

    emacs config ++

--- emacs/init.el
+++ emacs/init.el
@@ -15,6 +15,7 @@
 (set-frame-font "Monaco 11" nil t)
 
 ;; graphics
+(menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (blink-cursor-mode 0)
@@ -25,10 +26,19 @@
 (setq scroll-conservatively most-positive-fixnum)
 (setq display-line-numbers-width 3)
 (setq posframe-gtk-resize-child-frames 'resize-mode)
+(global-hl-line-mode +1)
 
 (use-package hybrid-reverse-theme
   :init
   (load-theme 'hybrid-reverse t))
+
+(use-package all-the-icons)
+
+;; gc
+
+(use-package gcmh
+  :config
+  (gcmh-mode +1))
 
 ;; search
 (setq completion-styles
@@ -46,13 +56,10 @@
 
 (use-package rg)
 
-(use-package gcmh
-  :init
-  (gcmh-mode +1))
-
 (use-package exec-path-from-shell
   :init
   (exec-path-from-shell-initialize))
+
 (use-package which-key
   :init
   (which-key-mode))
@@ -61,44 +68,50 @@
   :hook (after-init . doom-modeline-mode))
 
 (use-package evil
+  :custom
+  (evil-want-keybinding nil)
   :init
   (evil-mode +1))
 
+(use-package evil-collection
+  :after evil
+  :init
+  (evil-collection-init))
+
 (use-package dashboard
+  :diminish
   :custom
   (dashboard-banner-logo-title "Hejka")
   (dashboard-startup-banner 'logo)
-  (dashboard-items '((projects . 5)
-		    (recents . 5)))
+  (dashboard-items '((projects . 10)
+		    (recents . 10)))
   :init
   (dashboard-setup-startup-hook))
 
 (use-package projectile
   :bind
   (:map projectile-mode-map
-	("C-c p" . projectile-command-map))
+       ("C-c p" . projectile-command-map))
   :init
   (projectile-mode +1))
 
-(use-package treemacs
-  :defer t
+(use-package neotree
+  :config
+  (setq projectile-switch-project-action `neotree-projectile-action))
+
+(use-package centaur-tabs
+  :hook (dashboard-mode . centaur-tabs-local-mode)
   :custom
-  (treemacs-hide-gitignored-files-mode nil)
-  :bind
-  (:map global-map
-        ("M-0"       . treemacs-select-window)
-        ("C-x t 1"   . treemacs-delete-other-windows)
-        ("C-x t t"   . treemacs)
-        ("C-x t d"   . treemacs-select-directory)
-        ("C-x t B"   . treemacs-bookmark)
-        ("C-x t C-t" . treemacs-find-file)
-        ("C-x t M-t" . treemacs-find-tag)))
-
-(use-package treemacs-evil
-  :after (treemacs evil))
-
-(use-package treemacs-projectile
-  :after (treemacs projectile))
+  (uniquify-separator "/")
+  (centaur-tabs-style "bar")
+  (centaur-tabs-height 32)
+  (centaur-tabs-set-icons t)
+  (centaur-tabs-set-modified-marker t)
+  (centaur-tabs-show-navigation-buttons t)
+  (x-underline-at-descent-line t)
+  :config
+  (centaur-tabs-mode t)
+  (centaur-tabs-group-by-projectile-project))
 
 (use-package evil-nerd-commenter
   :bind ("M-/" . evilnc-comment-or-uncomment-lines))
@@ -118,12 +131,20 @@
 
 (use-package lsp-mode
   :commands (lsp lsp-deferred)
+  :hook
+  (lsp-mode . lsp-enable-which-key-integration)
   :custom
   (lsp-keymap-prefix "C-c l")
   (lsp-enable-suggest-server-download nil)
-  :init
+  ;;ui
+  (lsp-eldoc-enable-hover nil)
+  (lsp-signature-auto-activate nil)
   :config
-  (lsp-enable-which-key-integration t))
+  (evil-define-key 'normal 'global (kbd "gd") 'lsp-find-definition)
+  (evil-define-key 'normal 'global (kbd "ga") 'lsp-execute-code-action)
+  (evil-define-key 'normal 'global (kbd "<leader>mv") 'lsp-rename)
+  :init
+  (setq lsp-auto-execute-action nil))
 
 (use-package lsp-ui
   :hook (lsp-mode . lsp-ui-mode)
@@ -139,3 +160,17 @@
 
 (use-package rustic)
 (use-package toml-mode)
+
+;; notes
+;; lsp-ui-flycheck-list - show errors
+
+;; bindings
+(evil-set-leader 'normal (kbd "SPC"))
+(evil-define-key 'normal 'global (kbd "<leader>nn") 'neotree-toggle)
+(evil-define-key 'normal 'global (kbd "zs") 'save-buffer)
+(evil-define-key 'normal 'global (kbd "<leader>pp") 'projectile-switch-project)
+
+(evil-define-key 'normal 'global (kbd "<leader>h") 'windmove-left)
+(evil-define-key 'normal 'global (kbd "<leader>j") 'windmove-down)
+(evil-define-key 'normal 'global (kbd "<leader>k") 'windmove-up)
+(evil-define-key 'normal 'global (kbd "<leader>l") 'windmove-right)
