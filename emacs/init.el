82b575d406d2afd8941d2ed2ec9dd0e8878fbf3b
Marcin Pajkowski <marcin.pajkowski@gmail.com>
2022-12-03 02:07:17 +0100

    Sun Dec  4 07:59:25 AM CET 2022

--- emacs/init.el
+++ emacs/init.el
@@ -1,53 +1,7 @@
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 6))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
-(when (eq system-type 'darwin)
-  (customize-set-variable 'native-comp-driver-options '("-Wl,-w"))
-  (setq mac-option-modifier 'alt)
-  (setq mac-command-modifier 'meta)
-  (add-hook 'window-setup-hook 'toggle-frame-maximized t))
-
-(straight-use-package 'use-package)
-(setq straight-use-package-by-default +1)
-
-;; utf8
-(set-charset-priority 'unicode)
-(setq locale-coding-system   'utf-8)
-(set-terminal-coding-system  'utf-8)
-(set-keyboard-coding-system  'utf-8)
-(set-selection-coding-system 'utf-8)
-(prefer-coding-system        'utf-8)
-(setq default-process-coding-system '(utf-8-unix . utf-8-unix))
-(setq auto-window-vscroll nil)
-
-;; kll term on exit
-(defadvice term-handle-exit
-  (after term-kill-buffer-on-exit activate)
-(kill-buffer))
-
-(use-package exec-path-from-shell
-  :init
-  (exec-path-from-shell-initialize))
-
-;; font
-(when (display-graphic-p)
-    (if (eq system-type 'darwin)
-    (set-face-attribute 'default nil :weight 'light :font "Monaco" :height 140)
-    (set-face-attribute 'default nil :weight 'bold :font "Monaco" :height 120 )))
-
 ;; ui/ux global settings
 (when (eq system-type 'gnu/linux)
   (menu-bar-mode -1))
+(add-hook 'window-setup-hook 'toggle-frame-maximized t)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (blink-cursor-mode 0)
@@ -75,6 +29,64 @@
 (global-auto-revert-mode t)
 (setq auto-revert-verbose nil)
 
+;; autosave
+(setq backup-by-copying t
+      backup-directory-alist '(("." . "~/.emacs-saves"))
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)
+(setq auto-save-file-name-transforms
+      `((".*" "~/.emacs-saves/" t)))
+
+;; font
+(when (display-graphic-p)
+    (if (eq system-type 'darwin)
+    (set-face-attribute 'default nil :weight 'light :font "Monaco" :height 140)
+    (set-face-attribute 'default nil :weight 'normal :font "Monaco" :height 120 )))
+
+;; utf8
+(set-charset-priority 'unicode)
+(setq locale-coding-system   'utf-8)
+(set-terminal-coding-system  'utf-8)
+(set-keyboard-coding-system  'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system        'utf-8)
+(setq default-process-coding-system '(utf-8-unix . utf-8-unix))
+(setq auto-window-vscroll nil)
+
+;; mac keys
+(when (eq system-type 'darwin)
+  (customize-set-variable 'native-comp-driver-options '("-Wl,-w"))
+  (setq mac-option-modifier 'alt)
+  (setq mac-command-modifier 'meta))
+
+;; initialize straight
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default +1)
+
+;; kill term on exit
+(defadvice term-handle-exit
+  (after term-kill-buffer-on-exit activate)
+(kill-buffer))
+
+(use-package exec-path-from-shell
+  :init
+  (exec-path-from-shell-initialize))
+
 (use-package dired
   :straight (:type built-in)
   :config
@@ -82,10 +94,6 @@
   (put 'dired-find-alternate-file 'disabled nil)
   (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
   (define-key dired-mode-map (kbd "^") (lambda () (interactive) (find-alternate-file ".."))))
-
-(use-package hybrid-reverse-theme
-  :config
-  (load-theme 'hybrid-reverse t))
 
 ;; gc
 (use-package gcmh
@@ -100,8 +108,15 @@
   (vertico-mode +1))
 
 (use-package savehist
+  :straight (:type built-in)
   :init
   (savehist-mode))
+
+(use-package uniquify
+  :straight (:type built-in)
+  :config
+  (setq uniquify-separator "/"
+        uniquify-buffer-name-style 'forward))
 
 (use-package rg)
 
@@ -123,7 +138,13 @@
   (setq evil-want-integration t)
   (setq evil-undo-system 'undo-redo)
   :config
+  (evil-set-initial-state 'vterm-mode 'emacs)
   (evil-mode +1))
+
+(use-package evil-numbers
+  :config
+  (evil-define-key '(normal visual) 'global (kbd "C-c =") 'evil-numbers/inc-at-pt)
+  (evil-define-key '(normal visual) 'global (kbd "C-c -") 'evil-numbers/dec-at-pt))
 
 (use-package evil-collection
   :after (evil)
@@ -134,11 +155,11 @@
 
 (use-package projectile
   :straight (projectile :type git
-			:host github
-			:repo "bbatsov/projectile"
-			:fork (:host github
-			       :repo "mpajkowski/projectile"
-			       :branch "fix/1777"))
+                        :host github
+                        :repo "bbatsov/projectile"
+                        :fork (:host github
+                               :repo "mpajkowski/projectile"
+                               :branch "fix/1777"))
   :init
   (setq projectile-completion-system 'default)
   :config
@@ -168,6 +189,19 @@
   :config
   (treemacs-set-scope-type 'Perspectives))
 
+(use-package all-the-icons)
+
+(use-package doom-themes
+  :after (treemacs all-the-icons)
+  :config
+  (setq doom-themes-enable-bold nil
+        doom-theme-enable-italic nil)
+  (load-theme 'doom-tomorrow-night t)
+  (setq doom-themes-treemacs-theme "doom-colors")
+  (doom-themes-treemacs-config)
+  (doom-themes-org-config))
+
+
 (use-package evil-nerd-commenter
   :bind ("M-/" . evilnc-comment-or-uncomment-lines))
 
@@ -191,10 +225,11 @@
   (eglot-managed-mode . (lambda () (eldoc-mode -1)))
   :config
   (setq eglot-stay-out-of '(eldoc))
-  (setq-default eglot-workspace-configuration '(:rust-analyzer (:checkOnSave (:enable t
-                                                                      :command "clippy"
-                                                                      :extraArgs ["--target-dir" "/tmp/rust-analyzer-check"])
-                                                        :cargo (:features "all")))))
+  (setq-default eglot-workspace-configuration
+                '(:rust-analyzer (:checkOnSave (:enable t
+                                                :command "clippy"
+                                                :extraArgs ["--target-dir" "/tmp/rust-analyzer-check"])
+                                  :cargo (:features "all")))))
 
 (defun flymake-clear-diagnostics ()
   "Removes diagnostics list"
@@ -215,6 +250,7 @@
 
 (use-package vterm
   :defer t)
+
 (use-package restclient
   :defer t)
 
@@ -224,6 +260,7 @@
   (before-save . eglot-format-buffer)
   :mode ((rx ".rs" string-end) . rustic-mode)
   :config
+  (setq rustic-default-test-arguments "--benches --tests --all-features -- --nocapture")
   (setq rustic-lsp-client 'eglot))
 
 (use-package vimrc-mode
@@ -236,11 +273,11 @@
 (use-package osm
   :defer t
   :bind (("C-c m h" . osm-home)
-	 ("C-c m s" . osm-search)
-	 ("C-c m v" . osm-server)
-	 ("C-c m t" . osm-goto)
-	 ("C-c m x" . osm-gpx-show)
-	 ("C-c m j" . osm-bookmark-jump))
+         ("C-c m s" . osm-search)
+         ("C-c m v" . osm-server)
+         ("C-c m t" . osm-goto)
+         ("C-c m x" . osm-gpx-show)
+         ("C-c m j" . osm-bookmark-jump))
 
   :config
   (setq osm-server 'default)
